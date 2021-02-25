9030db5983addc5bc78c2b1b956e20db68420f1a
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-02-25 09:42:44 -0500

    added prisma

--- src/data/choices.sql
+++ src/data/choices.sql
@@ -1,5 +1,5 @@
-insert into choice (id, description, pick) values (1, 'Realigned value-added data-warehouse', false);
-insert into choice (id, description, pick) values (2, 'Innovative mission-critical alliance', true);
-insert into choice (id, description, pick) values (3, 'Pre-emptive holistic emulation', false);
-insert into choice (id, description, pick) values (4, 'Seamless client-driven matrices', false);
-insert into choice (id, description, pick) values (5, 'Diverse actuating application', true);
+insert into choices (id, detail, pick) values (1, 'Realigned value-added data-warehouse', false);
+insert into choices (id, detail, pick) values (2, 'Innovative mission-critical alliance', true);
+insert into choices (id, detail, pick) values (3, 'Pre-emptive holistic emulation', false);
+insert into choices (id, detail, pick) values (4, 'Seamless client-driven matrices', false);
+insert into choices (id, detail, pick) values (5, 'Diverse actuating application', true);
--- src/data/projects.sql
+++ src/data/projects.sql
@@ -1,5 +1,5 @@
-insert into projects (id, name, description, duration, date added) values (1, 'Operative', 'Re-contextualized content-based info-mediaries', '5:03 PM', '10/5/2020');
-insert into projects (id, name, description, duration, date added) values (2, 'secondary', 'Re-contextualized homogeneous throughput', '12:19 AM', '2/9/2021');
-insert into projects (id, name, description, duration, date added) values (3, 'Seamless', 'Cloned disintermediate database', '3:24 PM', '9/21/2020');
-insert into projects (id, name, description, duration, date added) values (4, 'Down-sized', 'Polarised clear-thinking flexibility', '12:38 PM', '6/18/2020');
-insert into projects (id, name, description, duration, date added) values (5, 'Exclusive', 'Re-engineered web-enabled instruction set', '3:42 PM', '3/22/2020');
+insert into projects (id, name, description, duration, date_added) values (1, 'Operative', 'Re-contextualized content-based info-mediaries', '5:03 PM', '10/5/2020');
+insert into projects (id, name, description, duration, date_added) values (2, 'secondary', 'Re-contextualized homogeneous throughput', '12:19 AM', '2/9/2021');
+insert into projects (id, name, description, duration, date_added) values (3, 'Seamless', 'Cloned disintermediate database', '3:24 PM', '9/21/2020');
+insert into projects (id, name, description, duration, date_added) values (4, 'Down-sized', 'Polarised clear-thinking flexibility', '12:38 PM', '6/18/2020');
+insert into projects (id, name, description, duration, date_added) values (5, 'Exclusive', 'Re-engineered web-enabled instruction set', '3:42 PM', '3/22/2020');
